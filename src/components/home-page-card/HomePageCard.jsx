9be84346ee28d56954f64782d6c70935e08a37c8
Laima Petrauskiene <laima@tinklai.net>
2022-02-05 20:07:47 +0200

    HomePageCard created

--- src/components/home-page-card/HomePageCard.jsx
+++ src/components/home-page-card/HomePageCard.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Paper, Box, TextField, Typography } from "@mui/material";
+
+import BasicDatePicker from "./DatePicker";
 
 const HomePageCard = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <Box
+      sx={{
+        width: 400,
+
+        position: "absolute",
+        top: 300,
+        left: 250,
+        margin: 2,
+        padding: 2,
+      }}
+    >
+      <Paper elevation={3}>
+        <Typography variant="h5" sx={{ padding: 2 }}>
+          Best solution for your yacht charter
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 2,
+            margin: 2,
+            flexDirection: "column",
+          }}
+        >
+          <TextField
+            sx={{ width: "100%" }}
+            id="outlined-basic"
+            label="Place of departure"
+            variant="outlined"
+          >
+            Place of departure
+          </TextField>
+          <Box sx={{ display: "flex", gap: 3, mb: 2 }}>
+            <BasicDatePicker />
+            <BasicDatePicker />
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
+  );
 };
 
 export default HomePageCard;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+
 import Navbar from "./navbar";
+import HomePageCard from "../home-page-card/HomePageCard";
 
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HomePageCard/>
     </div>
   );
 };
